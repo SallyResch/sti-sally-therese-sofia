12d1efe0be72e98e4b196041e478c3bc692de29b
SallyResch <sallyresch27@gmail.com>
2022-02-01 13:13:27 +0100

    add logger

---
+++ sti-sally-therese-sofia-domain/src/main/java/sti/sally/therese/sofia/domain/Main.java
@@ -0,0 +1,12 @@
+package sti.sally.therese.sofia.domain;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Vilket personnummer har du?");
+        Scanner s = new Scanner(System.in);
+        int persNumber = s.nextInt();
+        //System.out.println("Okej, du heter....")
+    }
+}
--- sti-sally-therese-sofia-service-impl/src/main/java/sti/sally/therese/sofia/service/StiServiceImpl.java
+++ sti-sally-therese-sofia-service-impl/src/main/java/sti/sally/therese/sofia/service/StiServiceImpl.java
@@ -1,13 +1,19 @@
 package sti.sally.therese.sofia.service;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import sti.sally.therese.sofia.domain.Courses;
 import sti.sally.therese.sofia.domain.Students;
 import sti.sally.therese.sofia.domain.Teacher;
 
 public class StiServiceImpl implements StiService {
 
+    private static final Logger LOGGER= LoggerFactory.getLogger(StiServiceImpl.class);
+
+
     @Override
     public Students createStudent(String givenName, String surName, int courses) {
+        LOGGER.trace("created student successfully");
         return null /* eller att det händer något annat? */;
     }
 
